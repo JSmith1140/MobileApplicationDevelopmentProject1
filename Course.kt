ce2ad584d8bafdee0ba810b804b6e499ccbd496b
AntonRyan1 <ryanan@merrimack.edu>
2026-02-02 17:53:28 -0500

    Course.kt

---
+++ Course.kt
@@ -0,0 +1,35 @@
+package com.maxli.coursegpa
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Index
+
+
+@Entity(
+    tableName = "courses",
+    indices = [Index(value = ["courseName"], unique = true)]
+)
+class Course {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "courseId")
+    var id: Int = 0
+
+    @ColumnInfo(name = "courseName")
+    var courseName: String = ""
+
+    @ColumnInfo(name = "creditHour")
+    var creditHour: Int = 0
+
+    @ColumnInfo(name = "letterGrade")
+    var letterGrade: String = ""
+
+    constructor() {}
+
+    constructor(coursename: String, creditHour: Int, letterGrade: String) {
+        this.courseName = coursename
+        this.creditHour = creditHour
+        this.letterGrade = letterGrade
+    }
+}
